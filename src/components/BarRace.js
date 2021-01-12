3ebe03cae9e29a7642a83a3d59c7999b38d20eeb
Azuk 443 <me@azuk.top>
2021-01-12 19:10:11 +0800

    add template

---
+++ src/components/BarRace.js
@@ -0,0 +1,9 @@
+import BarRaceTest from "pages/tests/barRaceTest";
+
+export default function BarRace(props) {
+    return (
+        <div>
+            {props.info}
+        </div>
+    );
+}
---
+++ src/pages/tests/BarRaceTest.js
@@ -0,0 +1,7 @@
+import BarRace from 'components/BarRace';
+
+export default function BarRaceTest() {
+    return (
+        <BarRace info="test" />
+    );
+}
