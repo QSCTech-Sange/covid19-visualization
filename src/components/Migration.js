d17212b32355e0f9d4377e016c02a85c617a3c24
NCJ <iamncj@outlook.com>
2021-01-15 17:06:41 +0800

    added AntV L7 Map
    disabled SSR on this page

--- src/components/Migration.js
+++ src/components/Migration.js
@@ -1,14 +1,22 @@
-import ReactEcharts from "echarts-for-react";
-import {useState} from "react";
+import dynamic from "next/dynamic";
+
+// disable ssr on AntV
+const MapboxScene = dynamic(() => import("@antv/l7-react/lib/component/MapboxScene"), { ssr: false });
+
+import {useState, useEffect} from "react";
 
 export default function Migration(props) {
-    let [option, SetOption] = useState(undefined);
 
     return (
         <div className="migration">
-            {option && (
-                <ReactEcharts option={option} style={{ height: props.height ?? 400 }} />
-            )}
+            <MapboxScene
+                option={{}}
+                map={{
+                    style: 'dark',
+                    center: [112, 20],
+                    token: 'pk.eyJ1IjoiemVuZ2Nob25nIiwiYSI6ImNrankxejBrMTA0ajYydXA4eXE4YmhnN2MifQ.0NVFgToOPeT5WKKZjC0--A',
+                }}
+            />
         </div>
     );
 }
